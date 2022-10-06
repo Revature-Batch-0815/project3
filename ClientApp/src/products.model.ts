0af8d4a07707b7a0bf987b67b852638bf1f00054
aquamanly <aharnold@gmail.com>
2022-10-06 17:27:21 -0400

    The Product List

---
+++ ClientApp/src/products.model.ts
@@ -0,0 +1,14 @@
+//namespace project3.ClientApp.src
+//{
+
+//}
+
+export const Product = {
+  productId: "",
+  productName: "",
+  productCategory: "",
+  productDescription: "",
+  productImgUrl: "",
+  productPrice: "",
+  productQty: ""
+}
