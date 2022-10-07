8613b2443bf989556e22eacfa94e8ca843ad36b3
aquamanly <aharnold@gmail.com>
2022-10-07 13:10:40 -0400

    Finished Complete connection for the products connection

--- ClientApp/src/app/Components/product-list/product-list.component.ts
+++ ClientApp/src/app/Components/product-list/product-list.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { Product } from 'src/products.model';
+import { AppServiceService } from '../../Services/app-services.service';
+
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-product-list',
@@ -8,52 +11,15 @@ import { Product } from 'src/products.model';
 })
 export class ProductListComponent implements OnInit {
 
-  Products: typeof Product[] = [
-    {
-      productId: "1",
-      productName: "Les hautes solitudes",
-      productCategory: "",
-      productDescription: "Morbi vel lectus in quam fringilla rhoncus. Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis.",
-      productImgUrl: "http://dummyimage.com/100x100.png/cc0000/ffffff",
-      productPrice: "$79.69",
-      productQty: "49"
-    },
-    {
-      productId: "2",
-      productName: "Bohème, La",
-      productCategory: "Drama|Romance",
-      productDescription: "Proin risus. Praesent lectus. Vestibulum quam sapien, varius ut, blandit non, interdum in, ante. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis faucibus accumsan odio.",
-      productImgUrl: "http://dummyimage.com/100x100.png/dddddd/000000",
-      productPrice: "$60.14",
-      productQty: "16"
-    },
-    {
-      productId: "3",
-      "productName": "Garden of Eden, The", "productCategory": "Drama", "productDescription": "Curabitur in libero ut massa volutpat convallis. Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo.", "productImgUrl": "http://dummyimage.com/100x100.png/ff4444/ffffff", "productPrice": "$47.53",
-      "productQty": "3"
-    },
-    {
-      productId: "4",
-      "productName": "Prairie Love",
-      "productCategory": "Comedy|Drama",
-      "productDescription": "Proin leo odio, porttitor id, consequat in, consequat ut, nulla. Sed accumsan felis. Ut at dolor quis odio consequat varius. Integer ac leo. Pellentesque ultrices mattis odio. Donec vitae nisi. Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus. Nulla suscipit ligula in lacus. Curabitur at ipsum ac tellus semper interdum.",
-      "productImgUrl": "http://dummyimage.com/100x100.png/ff4444/ffffff",
-      "productPrice": "$56.94",
-      "productQty": "83"
-    },
-    {
-      productId: "5",
-      "productName": "Manhattan Baby",
-      "productCategory": "Horror",
-      "productDescription": "Integer ac leo.",
-      "productImgUrl": "http://dummyimage.com/100x100.png/dddddd/000000",
-      "productPrice": "$65.40",
-      "productQty": "83"
-    }
-  ]
-  constructor() { }
-  p = this.Products;
+  Products: Product[] = [];
+ 
+
+  constructor(private productService: AppServiceService) { }
+
+
   ngOnInit(): void {
+    this.productService.getProducts().subscribe((results: Product[]) => (this.Products = results));
   }
+
 
 }
--- Program.cs
+++ Program.cs
@@ -39,7 +39,7 @@ else
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
+app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
