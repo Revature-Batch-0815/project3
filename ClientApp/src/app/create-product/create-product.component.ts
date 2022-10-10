4d3b33c87af71688cc2dda22ab40fb0cd77c311a
Alec Battest <AlecBattest@gmail.com>
2022-10-10 16:07:57 -0500

    Made create product page presentable. Added form validations. I believe it is functionally ready to be connected to the database.

--- ClientApp/src/app/create-product/create-product.component.ts
+++ ClientApp/src/app/create-product/create-product.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { AppServiceService } from 'src/app/Services/app-services.service';
+
 
 @Component({
   selector: 'app-create-product',
@@ -7,9 +11,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateProductComponent implements OnInit {
 
-  constructor() { }
+  createProductForm: FormGroup = new FormGroup({});
+
+  constructor(private formBuilder: FormBuilder, private appService: AppServiceService, private _snackbar: MatSnackBar) { }
 
   ngOnInit(): void {
+    this.createProductForm = this.formBuilder.group({
+      'name': new FormControl('', [Validators.required, Validators.minLength(5)]),
+      'category': new FormControl('', [Validators.required]),
+      'description': new FormControl('', [Validators.required, Validators.minLength(5)]),
+      'imageurl': new FormControl('', [Validators.required]),
+      'price': new FormControl('', [Validators.required]),
+      'quantity': new FormControl('', [Validators.required])
+    })
+  }
+
+  addProductsApi() {
+    this.appService.addProductsApi(this.createProductForm.value).subscribe(data => {
+      this._snackbar.open('Product created successfully')
+    }, err => {
+      this._snackbar.open('Product creation unsuccessful')
+    })
   }
 
 }
