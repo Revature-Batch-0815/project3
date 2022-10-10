c2ecdfb1666b2e0a4d982a629aa301a3b4c2d2df
hasnell <nas6164000@gmail.com>
2022-10-10 12:10:13 -0400

    Details Page Now Uses API

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -35,8 +35,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSliderModule } from '@angular/material/slider';
@@ -46,20 +44,16 @@ import { MatMenuModule } from '@angular/material/menu';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 // Material Layout
-import { MatCardModule } from '@angular/material/card';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatGridListModule } from '@angular/material/grid-list';
-import { MatListModule } from '@angular/material/list';
 import { MatStepperModule } from '@angular/material/stepper';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatTreeModule } from '@angular/material/tree';
 // Material Buttons & Indicators
-import { MatButtonModule } from '@angular/material/button';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatBadgeModule } from '@angular/material/badge';
 import { MatChipsModule } from '@angular/material/chips';
-import { MatIconModule } from '@angular/material/icon';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatRippleModule } from '@angular/material/core';
@@ -71,7 +65,6 @@ import { MatTooltipModule } from '@angular/material/tooltip';
 // Material Data tables
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
-import { MatTableModule } from '@angular/material/table';
 import { ProductDetailsComponent } from './Components/product-details/product-details.component';
 
 
@@ -84,9 +77,9 @@ import { ProductDetailsComponent } from './Components/product-details/product-de
     CounterComponent,
     FetchDataComponent,
 
-    CreateProductComponent
+    CreateProductComponent,
 
-    EditProductComponent
+    EditProductComponent,
     ProductListComponent,
     ProductDetailsComponent
 
@@ -147,7 +140,7 @@ import { ProductDetailsComponent } from './Components/product-details/product-de
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent, canActivate: [AuthorizeGuard] },
 
-      { path:'pdetails', component:ProductDetailsComponent },
+      { path:'pdetails/:id', component:ProductDetailsComponent },
       { path: 'product', component: ProductListComponent },
 
     ]),
