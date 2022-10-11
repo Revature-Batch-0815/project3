37549679ab95c5872aa98e78c2de3988e1dfbd40
dukefan5656 <cbrudder84@gmail.com>
2022-10-11 14:48:47 -0700

    Revert "merged with main"
    This reverts commit 7ab7a901a7cbec6e68088e75d2e2ac8fbfb9ef2b, reversing
    changes made to f279f7e74ba6bf005a1c0dbe0d1afe28e8518ac3.

--- ClientApp/src/app/nav-menu/nav-menu.component.ts
+++ ClientApp/src/app/nav-menu/nav-menu.component.ts
@@ -3,11 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-nav-menu',
   templateUrl: './nav-menu.component.html',
-  styleUrls: ['./nav-menu.component.css'],
+  styleUrls: ['./nav-menu.component.css']
 })
 export class NavMenuComponent {
   isExpanded = false;
-  searchText: string = '';
 
   collapse() {
     this.isExpanded = false;
@@ -15,10 +14,5 @@ export class NavMenuComponent {
 
   toggle() {
     this.isExpanded = !this.isExpanded;
-  }
-
-  onSearchTextEntered(searchValue: string) {
-    this.searchText = searchValue;
-    console.log(this.searchText);
   }
 }
