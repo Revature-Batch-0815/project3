bbbd614d4d59682c24aa4273406e66eb0fb20bd2
Alec Battest <AlecBattest@gmail.com>
2022-10-14 00:15:26 -0500

    Added search bar to sort sidebar

--- ClientApp/src/app/Components/product-list/product-list.component.ts
+++ ClientApp/src/app/Components/product-list/product-list.component.ts
@@ -3,7 +3,7 @@ import { Product } from 'src/products.model';
 import { AppServiceService } from '../../services/app-services.service';
 
 import { HttpClient } from '@angular/common/http';
-import { FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, FormRecord } from '@angular/forms';
 
 @Component({
   selector: 'app-product-list',
@@ -23,15 +23,36 @@ export class ProductListComponent implements OnInit {
   form!: FormGroup;
   filteredProducts: Product[] = [];
 
+  productBank: Product[] = [];
+  private _searchTerm: string = '';
+
+  get searchTerm(): string { return this._searchTerm; }
+  set searchTerm(value: string) {
+    this._searchTerm = value;
+    this.filteredProducts = this.productBank.slice(0);
+    this.filteredProducts = this.filterPro(value);
+  }
+
+  filterPro(searchString: string) {
+    return this.filteredProducts.filter(product =>
+      product.productName.toLowerCase().indexOf(searchString.toLowerCase()) !== -1 ||
+      product.productDescription.toLowerCase().indexOf(searchString.toLowerCase()) !== -1);
+  }
+
+
+/*  private _pricePick: number = 0;*/
+
   constructor(private productService: AppServiceService, fb: FormBuilder) {
     this.form = fb.group({
-      selectedCategories: new FormArray([])
+      selectedCategories: new FormArray([]),
+      pricePick: new FormArray([])
     });
   }
 
   ngOnInit(): void {
     this.productService.getProducts().subscribe((results: Product[]) => (this.Products = results));
     this.productService.getProducts().subscribe((results: Product[]) => (this.filteredProducts = results));
+    this.productService.getProducts().subscribe((results: Product[]) => (this.productBank = results));
   }
 
   onCheckboxChange(event: any) {
@@ -47,20 +68,26 @@ export class ProductListComponent implements OnInit {
 
   submit() {
     const selectedCategories = (this.form.controls['selectedCategories'] as FormArray);
+    //const pricePick = (this.form.controls['pricePick']);
+    //console.log(pricePick.value);
     if (selectedCategories.length == 1) {
       this.filteredProducts = this.Products.filter(item => item.productCategory == selectedCategories.value[0]);
+      this.productBank = this.filteredProducts.slice(0);
       console.log(this.filteredProducts);
     }
     else if (selectedCategories.length == 2) {
       this.filteredProducts = this.Products.filter(item => item.productCategory == selectedCategories.value[0] || item.productCategory == selectedCategories.value[1]);
+      this.productBank = this.filteredProducts.slice(0);
       console.log(this.filteredProducts);
     }
     else if (selectedCategories.length == 3) {
       this.filteredProducts = this.Products.filter(item => item.productCategory == selectedCategories.value[0] || item.productCategory == selectedCategories.value[1] || item.productCategory == selectedCategories.value[2]);
+      this.productBank = this.filteredProducts.slice(0);
       console.log(this.filteredProducts);
     }
     else {
       this.productService.getProducts().subscribe((results: Product[]) => (this.filteredProducts = results));
+      this.productBank = this.filteredProducts.slice(0);
     }
   }
 }
