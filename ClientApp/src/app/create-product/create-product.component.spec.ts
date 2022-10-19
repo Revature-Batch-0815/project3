ade5043bcb5918c762dac56e8a65bb922a2ba7b5
dukefan5656 <cbrudder84@gmail.com>
2022-10-19 05:25:09 -0700

    added tests for Create Product Component

--- ClientApp/src/app/create-product/create-product.component.spec.ts
+++ ClientApp/src/app/create-product/create-product.component.spec.ts
@@ -2,9 +2,9 @@ import { RouterTestingModule } from "@angular/router/testing";
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Router } from '@angular/router';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, fakeAsync } from '@angular/core/testing';
 import { CreateProductComponent } from './create-product.component';
-import { FormsModule } from '@angular/forms'
+import { FormBuilder, FormsModule } from '@angular/forms'
 import { ReactiveFormsModule } from '@angular/forms'
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { TextFieldModule } from '@angular/cdk/text-field';
@@ -24,7 +24,10 @@ describe('CreateProductComponent', () => {
         MatSnackBarModule,
         TextFieldModule
       ],
-      declarations: [ CreateProductComponent ]
+      declarations: [CreateProductComponent],
+      providers: [
+        FormBuilder
+      ]
     })
     .compileComponents();
     httpClient = TestBed.get(HttpClient);
@@ -32,9 +35,28 @@ describe('CreateProductComponent', () => {
     fixture = TestBed.createComponent(CreateProductComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  });
+  }); //, category, description, imageurl, price, quantity
+  function updateForm(name: string, category: string, description: string, imageurl: string,
+                      price: number, quantity: number  ) {
+    component.createProductForm.controls['name'].setValue(name);
+    component.createProductForm.controls['category'].setValue(category);
+    component.createProductForm.controls['description'].setValue(description);
+    component.createProductForm.controls['imageurl'].setValue(imageurl);
+    component.createProductForm.controls['price'].setValue(price);
+    component.createProductForm.controls['quantity'].setValue(quantity);
 
+  }
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  it('form values should have Validators', fakeAsync(() => {
+    updateForm("iPad","Electronics","mock description","www.tech.com", 50, 6);
+    expect(component.createProductForm.hasValidator).toBeTruthy();
+  }));
+  it('form values should be correct type', fakeAsync(() => {
+    updateForm("iPad", "Electronics", "mock description", "www.tech.com", 50, 6);
+    expect(component.createProductForm.value).toEqual({
+      name: "iPad", category: "Electronics",
+      description: "mock description", imageurl: "www.tech.com", price: 50, quantity: 6 });
+  }));
 });
